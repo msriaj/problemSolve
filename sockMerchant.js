0925c5a1e0193e620df2ef8f17467c4f68223813
md-riaj11 <contact.riaj@gmail.com>
2022-08-13 01:16:09 +0600

    sockMerchant hackerrank

--- sockMerchant.js
+++ sockMerchant.js
@@ -1,3 +1,4 @@
+// https://www.hackerrank.com/challenges/sock-merchant/problem?isFullScreen=true 
 // // There is a large pile of socks that must be paired by color.
 // // Given an array of integers representing the color of each sock,
 // // determine how many pairs of socks with matching colors there are.
